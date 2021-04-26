24bf9383ab7058b87e55e9281264a86f7cbe77dc
Reeggie Mace <reginaldmace36@gmail.com>
2021-04-25 20:41:58 -0400

    Added validation to user input. Corrected file path to generateHTML.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateHTML = require("./lib/generateHTML");
 
 menu = () => {
   createManger = () => {
@@ -12,25 +13,64 @@ menu = () => {
           type: "input",
           name: "name",
           message: "Enter the manager name",
-          // validate (go to inquire doc)
+          validate: function (name) {
+            if (name) {
+              return true;
+            } else {
+              return "Employee name can't be empty";
+            }
+          },
         },
         {
           type: "input",
           name: "id",
           message: "What is the team manager's id?",
-          // validate (go to inquire doc)
+          validate: function (id) {
+            if (!id.isNaN) {
+              return true;
+            } else {
+              return "Please assign a number id for this employee";
+            }
+          },
         },
         {
           type: "input",
           name: "email",
           message: "What is the team manager's email?",
-          // validate (go to inquire doc)
+          validate: function (email) {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+
+            if (valid) {
+              return true;
+            } else {
+              return "Please enter a valid email address";
+            }
+          },
         },
         {
           type: "input",
           name: "officeNumber",
           message: "What is the team manager's office number?",
-          // validate (go to inquire doc)
+          validate: function (officeNumber) {
+            if (officeNumber) {
+              return true;
+            } else {
+              return "Employee must be assigned an office number";
+            }
+          },
+        },
+        {
+          type: "checkbox",
+          name: "addEmployee",
+          message: "Would you like to add another employee",
+          choices: ["yes", "no"],
+          validate: function (choices) {
+            if (choices) {
+              return true;
+            } else {
+              return "Please select an option to continue";
+            }
+          },
         },
       ])
       .then(({ name, id, email, officeNumber }) => {
@@ -45,34 +85,129 @@ menu = () => {
           type: "input",
           name: "name",
           message: "Enter the engineer's name",
-          // validate (go to inquire doc)
+          validate: function (name) {
+            if (name) {
+              return true;
+            } else {
+              return "Employee name can't be empty";
+            }
+          },
         },
         {
           type: "input",
           name: "id",
           message: "What is engineer's id?",
-          // validate (go to inquire doc)
+          validate: function (id) {
+            if (!id.isNaN) {
+              return true;
+            } else {
+              return "Please assign a number id for this employee";
+            }
+          },
         },
         {
           type: "input",
           name: "email",
           message: "What is the team engineer's email?",
-          // validate (go to inquire doc)
+          validate: function (email) {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+
+            if (valid) {
+              return true;
+            } else {
+              return "Please enter a valid email address";
+            }
+          },
         },
         {
           type: "input",
           name: "github",
           message: "What is the team engineer's github?",
-          // validate (go to inquire doc)
+          validate: function (name) {
+            if (name) {
+              return true;
+            } else {
+              return "Employee gitHub can't be empty";
+            }
+          },
         },
       ])
-      .then(({ name, id, email, officeNumber }) => {
+      .then(({ name, id, email, github }) => {
         const engineer = new Engineer(name, id, email, github);
         console.log(engineer);
       });
   };
+  createIntern = () => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Enter the intern's name",
+          validate: function (name) {
+            if (name) {
+              return true;
+            } else {
+              return "Employee name can't be empty";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is intern's id?",
+          validate: function (id) {
+            if (!id.isNaN) {
+              return true;
+            } else {
+              return "Please assign a number id for this employee";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is the intern's email?",
+          validate: function (email) {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+
+            if (valid) {
+              return true;
+            } else {
+              return "Please enter a valid email address";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "What school is the intern enrolled",
+          validate: function (name) {
+            if (name) {
+              return true;
+            } else {
+              return "Intern's school can't be empty";
+            }
+          },
+        },
+      ])
+      .then(({ name, id, email, school }) => {
+        const intern = new Engineer(name, id, email, school);
+        console.log(intern);
+      });
+  };
   createManger();
-  //createEngineer();
+  // createEngineer();
+  // createIntern();
 };
+// TODO: Create a function to write HTML file
+
+// function init() {
+//   inquirer.prompt().then((response) => {
+//     const html = generateHTML(response);
+//     console.log(html);
+//     writeToFile("index.html", index);
+//   });
+// }
 
 menu();
