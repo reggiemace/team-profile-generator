d2d34f29f115afa3560213ce837b88583d419684
Reeggie Mace <reginaldmace36@gmail.com>
2021-05-02 12:15:23 -0400

    successfully created html, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # team-profile-generator
+
+## Description:
+
 This application allows a manager to enter from the command-line, information about his/her employees, that will generate a HTML webpage that will display the summaries for each employee.
+
+## Installation
+
+This application runs in using the CLI. The user must type in npm index start the application. The user will then be prompted with a series of questions as it related to his role and the role of employees should the user choose to add employees to the profile
+
+The walk through vide can be seen here: https://youtu.be/2GRuyqOPH0s
+
+## License
+
+This application is license through MIT. You can find more out more about the license at : [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT).
+
+## Usage
+
+![image] (/Assets/images/Screen Shot 2021-05-02 at 12.07.03 PM.png?raw=true)
+
+
+## Test
+
+There are 4 test classes to test each employee object and variables that was created during this project. To run the test use npm run test.
+
+## Questions:
+
+For any questions about this application please contact me github: https://github.com/reggiemace
+Additional questions can be sent to mailto:reginaldmace36@gmail.com.
--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateHTML = require("./lib/generateHTML");
+//const generateHTML = require("./lib/generateHTML");
+//const getEmployee = require("./lib/generateHTML");
+
+let managerString = " ";
+let engineerString = " ";
+let internString = " ";
 
 menu = () => {
   createManger = () => {
@@ -59,25 +64,66 @@ menu = () => {
             }
           },
         },
-        {
-          type: "checkbox",
-          name: "addEmployee",
-          message: "Would you like to add another employee",
-          choices: ["yes", "no"],
-          validate: function (choices) {
-            if (choices) {
-              return true;
-            } else {
-              return "Please select an option to continue";
-            }
-          },
-        },
       ])
       .then(({ name, id, email, officeNumber }) => {
         const manager = new Manager(name, id, email, officeNumber);
-        console.log(manager);
+        console.info(manager);
+        managerString += `
+        <div class="employee-section">
+                    <div class="card my-card" style="width: 18rem;" >
+                        <div class="card-body">
+                            <div class="card-header">
+                                <h5 class="employee-name">${manager.name}</h5>
+                                <h6 class="manager-position mb-2 text-muted">${manager.getRole()}</h6>
+                            </div>
+                            <div class="card-content">
+                            <ul class="list-group">
+                            <li class="list-group-item">ID: ${manager.getId()}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${
+                              manager.email
+                            }">${manager.email}</a></li>
+                            <li class="list-group-item">Office Number: ${
+                              manager.officeNumber
+                            }</li>
+                        </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+        `;
+        newPrompt();
       });
   };
+
+  newPrompt = async () => {
+    const answer = await inquirer.prompt({
+      type: "list",
+      message: "Would you like to add an employee",
+      choices: ["yes", "no"],
+      name: "choice",
+    });
+    // if  eng -> create engineer
+    if (answer.choice == "yes") {
+      addEmployee();
+    } else if (answer.choice == "no") {
+      generateHTML();
+    }
+  };
+  addEmployee = async () => {
+    const answer = await inquirer.prompt({
+      type: "list",
+      message: "What type of employee would you like to add",
+      choices: ["Engineer", "Intern"],
+      name: "choice",
+    });
+    if (answer.choice == "Engineer") {
+      createEngineer();
+    } else if (answer.choice == "Intern") {
+      createIntern();
+    }
+  };
+
   createEngineer = () => {
     inquirer
       .prompt([
@@ -135,6 +181,32 @@ menu = () => {
       .then(({ name, id, email, github }) => {
         const engineer = new Engineer(name, id, email, github);
         console.log(engineer);
+        engineerString += `
+        <div class="employee-section">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <div class="card-header">
+                                <h5 class="employee-name">${engineer.name}</h5>
+                                <h6 class="engineer-position mb-2 text-muted">${engineer.getRole()}</h6>
+                            </div>
+                            <div class="card-content">
+                            <ul class="list-group">
+                            <li class="list-group-item">ID: ${engineer.getId()}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${
+                              engineer.email
+                            }">${engineer.email}</a></li>
+                            <li class="list-group-item">GitHub:<a href="https://github.com/${
+                              engineer.github
+                            }"></a></li>
+                        </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+        `;
+
+        newPrompt();
       });
   };
   createIntern = () => {
@@ -192,22 +264,77 @@ menu = () => {
         },
       ])
       .then(({ name, id, email, school }) => {
-        const intern = new Engineer(name, id, email, school);
+        const intern = new Intern(name, id, email, school);
         console.log(intern);
+        internString += `
+        <div class="employee-section container">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <div class="card-header">
+                                <h5 class="employee-name">${intern.name}</h5>
+                                <h6 class="intern-position mb-2 text-muted">${intern.getRole()}</h6>
+                            </div>
+                            <div class="card-content">
+                            <ul class="list-group">
+                            <li class="list-group-item">ID: ${intern.getId()}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${
+                              intern.email
+                            }>${intern.email}"</a>${intern.email}</li>
+                            <li class="list-group-item">School: ${
+                              intern.school
+                            }</li>
+                        </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+        `;
+        newPrompt();
       });
   };
   createManger();
-  // createEngineer();
-  // createIntern();
 };
-// TODO: Create a function to write HTML file
+function generateHTML() {
+  // const print = managerString + engineerString + internString; // make await if dnt work
+  // const oldList = fs.readFileSync("employeeList.html");
+  // print += oldList;
+  const html = `<!DOCTYPE html>
+    <html lang="en">
 
-// function init() {
-//   inquirer.prompt().then((response) => {
-//     const html = generateHTML(response);
-//     console.log(html);
-//     writeToFile("index.html", index);
-//   });
-// }
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Web Team Builder</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <link rel="stylesheet" href="Assets/css/style.css">
+    </head>
+
+    <body>
+
+        <div class="container-fluid">
+            <div>
+                <header>
+                    <h1>My Team</h1>
+                </header>
+            </div>
+            <main class="employee-list">
+            ${managerString}
+            ${engineerString}
+            ${internString}
+         </main>
+        </div>
+
+    </body>
+
+    </html>`;
+
+  fs.writeFile("index.html", html, (err) => {
+    if (err) throw err;
+  });
+}
 
 menu();
+// make sure emp exist ..and write to old list file
